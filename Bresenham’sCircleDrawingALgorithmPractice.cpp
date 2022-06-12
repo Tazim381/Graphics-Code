0f7b7e9861febd6adce191107c00dbbaf63ee0df
Tazim <70653485+Tazim381@users.noreply.github.com>
2022-06-12 23:34:03 +0600

    Add files via upload
    Bresenham's Circle Drawing algorithm

---
+++ Bresenham’sCircleDrawingALgorithmPractice.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+#define ll long long
+void drawCircle(int x,int y,int xc,int yc)
+{
+    putpixel(x+xc,y+yc,RED);
+    putpixel(y+xc,x+yc,RED);
+    putpixel(x+xc,-y+yc,RED);
+    putpixel(y+xc,-x+yc,RED);
+    putpixel(-x+xc,-y+yc,RED);
+    putpixel(-y+xc,-x+yc,RED);
+    putpixel(-x+xc,y+yc,RED);
+    putpixel(-y+xc,x+yc,RED);
+    delay(100);
+}
+int main()
+{
+
+    int gd = DETECT, gm;
+	initgraph(&gd, &gm, "C:\\TC\\BGI");
+    ll i,j,m,na,b;
+    ll x,y,p,r,xc,yc;
+    cout<<"Enter co-ordinate x and y "<<endl;
+    cin>>xc>>yc;
+    cout<<"Enter radious r "<<endl;
+    cin>>r;
+
+    x =0;
+    y=r;
+    p= 3- 2*r;
+    while(x<=y)
+    {
+        drawCircle(x,y,xc,yc);
+        if(p<0)
+        {
+            p = p + (4*x)+6;
+        }
+        else{
+            y--;
+            p = p+ 4*(x-y)+10 ;
+        }
+
+        x++;
+    }
+
+}
+
